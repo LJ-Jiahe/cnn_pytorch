426c37e8d72f0ef704b88a70fb537a8e273cbb09
Li, Jiahe <eljayli@qq.com>
2019-09-25 10:20:03 -0500

    add cnn_dynamic

--- playground.py
+++ playground.py
@@ -1,7 +1,16 @@
 
 
 import config as cfg
-from utils import ImageDataset, CNN_Sequential
+from utils import ImageDataset, CNN_Sequential, CNN_Dynamic
+from torch import nn
 
-model = CNN_Sequential(3, 28, 10)
+channel_num = [3, 16, 64, 128]
+kernel_sizes = [3, 3, 3]
+strides = [1, 1, 1]
+paddings = [1, 1, 1]
+ac_funs = [nn.ReLU, nn.ReLU, nn.ReLU]
+img_size = 28
+n_classes = 10
+
+model = CNN_Dynamic(channel_num, kernel_sizes, strides, paddings, ac_funs, img_size, n_classes)
 print(model)
--- utils/network.py
+++ utils/network.py
@@ -58,7 +58,7 @@ class CNN_Sequential(nn.Module):
                        ac_fun=nn.ReLU)
         )
 
-        self.mlp = nn.Sequential(
+        self.fc = nn.Sequential(
             nn.Linear(128 * img_size * img_size, 1024),
             nn.Sigmoid(),
             nn.Linear(1024, n_classes)
@@ -68,7 +68,7 @@ class CNN_Sequential(nn.Module):
     def forward(self, x):
         x = self.conv(x)
         x = x.view(x.size(0), -1)
-        x = self.mlp(x)
+        x = self.fc(x)
 
         return x
 
@@ -88,6 +88,33 @@ def conv_layer(in_channels, out_channels, kernel_size, stride, padding, ac_fun):
 
 
 class CNN_Dynamic(nn.Module):
-    def __init__(self):
+    def __init__(self, channel_num, kernel_sizes, strides, paddings, ac_funs, 
+                 img_size, n_classes):
         super().__init__()
+        conv_parameters = zip(channel_num, channel_num[1:], kernel_sizes, strides, 
+                         paddings, ac_funs)
+        
+        conv_layers = [conv_layer(in_channels, out_channels, kernel_size, stride, padding, ac_fun)
+        for in_channels, out_channels, kernel_size, stride, padding, ac_fun in conv_parameters]
+
+        self.conv = nn.Sequential(*conv_layers)
+        
+        # fc_parameters = zip()
+        fc_layers = 
+        self.fc = nn.Sequential(
+            nn.Linear(channel_num[-1] * img_size * img_size, 1024),
+            nn.Sigmoid(),
+            nn.Linear(1024, n_classes)
+        )
+        
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+
         
